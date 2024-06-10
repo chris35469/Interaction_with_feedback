2c0b612411aac51ea22f768f9f6f947a28163a96
Chris <chris35469@gmail.com>
2024-06-10 13:43:01 -0500

    Update touch.py

--- touch.py
+++ touch.py
@@ -4,14 +4,12 @@ import time
 def touch_sensor_interaction():
     while True:
         touch_value = cyberpi.pocket.read_digital("s2")  
-        if touch_value >.5:
+        if touch_value > 0.5:
             cyberpi.led.on('green') 
             cyberpi.console.println("Touched!")
            
         else:
             cyberpi.led.on('red')  
             cyberpi.console.println("Not Touched")
-          
-        time.sleep(0.5) 
 
 touch_sensor_interaction()
